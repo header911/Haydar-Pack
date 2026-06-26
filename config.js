5303a1f6a18d8bde5c17017bd1b25174a94e8e4b
header911 <131144337+header911@users.noreply.github.com>
2026-06-26 22:41:29 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-https://script.google.com/macros/s/AKfycbztGlxieynseTe3h84ysPesgpuGBYDXmGbtuxb9mjrNyG2W4UVrHs6FsHj57QG7uTTVyg/exec
 // ضع رابط /exec الخاص بـ Google Apps Script هنا بعد نشر الـ Backend.
 // مثال: window.HP_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/XXXX/exec';
 window.HP_APPS_SCRIPT_URL = '';
